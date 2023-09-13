9881c9692b799f75efa617937ceac52500c29a73
Serhat Taştan <serhattastannn@gmail.com>
2023-09-14 02:58:33 +0300

    Product : Sale : Sector Controllers Added

---
+++ WebAPI/Controllers/SaleController.cs
@@ -0,0 +1,73 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaleController : ControllerBase
+    {
+        ISaleService _saleService;
+
+        public SaleController(ISaleService saleService)
+        {
+            _saleService = saleService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _saleService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int saleId)
+        {
+            var result = _saleService.GetById(saleId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("update")]
+        public IActionResult Update(Sale sale)
+        {
+            var result = _saleService.Update(sale);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Sale sale)
+        {
+            var result = _saleService.Add(sale);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete(int saleId)
+        {
+            var result = _saleService.Delete(saleId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
---
+++ WebAPI/Controllers/SectorController.cs
@@ -0,0 +1,73 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SectorController : ControllerBase
+    {
+        ISectorService _sectorService;
+
+        public SectorController(ISectorService sectorService)
+        {
+            _sectorService = sectorService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _sectorService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int sectorId)
+        {
+            var result = _sectorService.GetById(sectorId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("update")]
+        public IActionResult Update(Sector sector)
+        {
+            var result = _sectorService.Update(sector);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Sector sector)
+        {
+            var result = _sectorService.Add(sector);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete(int sectorId)
+        {
+            var result = _sectorService.Delete(sectorId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
