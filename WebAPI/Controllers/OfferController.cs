c726a02b6435fdb66d11ceedae05f1cd299e15ab
Serhat Taştan <serhattastannn@gmail.com>
2023-09-14 02:50:08 +0300

    OfferController Added

---
+++ WebAPI/Controllers/OfferController.cs
@@ -0,0 +1,73 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfferController : ControllerBase
+    {
+        IOfferService _offerService;
+
+        public OfferController(IOfferService offerService)
+        {
+            _offerService = offerService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _offerService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int offerId)
+        {
+            var result = _offerService.GetById(offerId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("update")]
+        public IActionResult Update(Offer offer)
+        {
+            var result = _offerService.Update(offer);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Offer offer)
+        {
+            var result = _offerService.Add(offer);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete(int offerId)
+        {
+            var result = _offerService.Delete(offerId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
