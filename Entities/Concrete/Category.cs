45543c541e401e4ec52144022da40707b7a2f852
Serhat Taştan <serhattastannn@gmail.com>
2023-09-14 03:06:04 +0300

    Hotfix

--- Entities/Concrete/Category.cs
+++ Entities/Concrete/Category.cs
@@ -2,7 +2,7 @@
 
 namespace Entities.Concrete
 {
-    public class Sector : IEntity
+    public class Category : IEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
